32eaa873b18eccf5cedbea47c437aefd4a0c7d31
Den <no@>
2024-03-23 20:41:55 +0300

    Часть 2 Изменение масштаба PgUp + PgDn

--- big_maps_api.py
+++ big_maps_api.py
@@ -5,29 +5,42 @@ import pygame
 import requests
 
 coords1, coords2, scale = map(float, input().split())
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords1},{coords2}&z={int(scale)}&l=sat"
-response = requests.get(map_request)
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+def get_picture(coords1, coords2, scale):
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords1},{coords2}&z={int(scale)}&l=sat"
+    response = requests.get(map_request)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
 
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
+map_file = get_picture(coords1, coords2, scale)
 # Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYUP and event.key == pygame.K_PAGEDOWN:
+            if scale > 1:
+                scale -= 1
+                map_file = get_picture(coords1, coords2, scale)
+                screen.blit(pygame.image.load(map_file), (0, 0))
+                pygame.display.flip()
+        if event.type == pygame.KEYUP and event.key == pygame.K_PAGEUP:
+            try:
+                map_file = get_picture(coords1, coords2, scale + 1)
+                scale += 1
+                screen.blit(pygame.image.load(map_file), (0, 0))
+                pygame.display.flip()
+            except Exception:
+                scale -= 1
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
